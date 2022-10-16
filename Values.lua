debcd414a87becd79d2b278e2e5c2d0c38e5baf3
Namer32 <97604254+Namer32@users.noreply.github.com>
2022-10-16 18:13:05 +0500

    Update Values.lua

--- Values.lua
+++ Values.lua
@@ -1,6 +1,13 @@
-{
-    Unlisted = false,
-    Name = {
-        "xXHypnOgErXx";
-    };
-};
+local lib = {};
+
+function lib.Unlisted()
+    return false;
+end;
+
+function lib.Banned()
+    return {
+        Name = {
+            "xXHypnOgErXx"
+        }
+    }
+end;
