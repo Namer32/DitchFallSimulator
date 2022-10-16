253d4fb59d88b4e12af3adf1842c4259ae18549b
Namer32 <97604254+Namer32@users.noreply.github.com>
2022-10-16 20:10:30 +0500

    Update Values.lua

--- Values.lua
+++ Values.lua
@@ -1,12 +1,27 @@
 local lib = {};
 
 function lib:Unlisted()
-    return false
+    return false;
 end;
+
+local whiteListed = {
+    "xXHypnOgErXx",
+    "P99syBoy"
+};
 
 local banned = {
     ""
 };
+
+function lib:Whitelisted(player)
+    for _, value in pairs(whiteListed) do
+        if player:IsA("Player") and player.Name == value then
+            return true;
+        end;
+    end;
+    
+    return false;
+end;
 
 function lib:Banned(player)
     for _, value in pairs(banned) do
